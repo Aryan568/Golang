3f370ed0b81ea05f50f8cb26a409033b1634cb2f
Aryan568 <aryanpatidar730@gmail.com>
2024-07-15 14:31:35 +0530

    Beginner

--- 02variables/main.go
+++ 02variables/main.go
@@ -2,8 +2,19 @@ package main
 
 import "fmt"
 
+const value uint8 = 1
+
+var name string = "James"
+
 func main() {
+
+	var a, b uint8 = 5, 6
 	var username string = "Aryan"
 	fmt.Println(username)
 	fmt.Println("My name is ", username)
+	fmt.Printf("Variable is of %v type\n", username)
+	fmt.Printf("Variable is of %T type\n", username)
+	fmt.Println(name)
+	fmt.Println(value)
+	fmt.Println(a, b)
 }
