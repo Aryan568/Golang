3b70abcb05ec8438897656610e5ad826cd29c987
Aryan568 <aryanpatidar730@gmail.com>
2024-07-15 14:57:58 +0530

    Beginner

--- 03userinput/main.go
+++ 03userinput/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	hellos := "Hello there!"
+	fmt.Println(hellos)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("What is your name?")
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Nice to meet you ", input)
+}
