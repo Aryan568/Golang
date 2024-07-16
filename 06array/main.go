d84a91f23dab11879c50da486323f19a77170c62
Aryan568 <aryanpatidar730@gmail.com>
2024-07-16 15:51:34 +0530

    Beginner

---
+++ 06array/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Arrays")
+
+	var cryptoList [4]string
+
+	cryptoList[0] = "BITCOIN"
+	// cryptoList[1] = "ETHEREUM"
+	// cryptoList[2] = "BINANCE"
+	cryptoList[3] = "SOLANA"
+
+	fmt.Println("CryptoList is \n", cryptoList)
+}
